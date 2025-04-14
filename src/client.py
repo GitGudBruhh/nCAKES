3dd29ded2ad99f46d8a09cb0a37f67b35a9425af
Challenger84 <reshwar2004@gmail.com>
2025-04-14 18:38:33 +0530

    client response to heartbeat finished

--- src/client.py
+++ src/client.py
@@ -7,8 +7,10 @@ class Client:
     def __init__(self):
         self.server_ip = "127.0.0.1"
         self.server_port = 8080
-        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.conn.connect((self.server_ip, self.server_port))
+        self.server_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_conn.connect((self.server_ip, self.server_port))
+
+        self.avail_chunks = {}
         
 
     def register_with_server(self):
@@ -20,9 +22,9 @@ class Client:
 
         msg = json.dumps(message)
 
-        self.conn.send(msg.encode("utf-8"))
+        self.server_conn.send(msg.encode("utf-8"))
 
-        response = self.conn.recv(1024)
+        response = self.server_conn.recv(1024)
         data = json.loads(response)
 
         print(data)
@@ -32,9 +34,9 @@ class Client:
         elif data["message_type"] == 721:
             raise(data["message_comment"])
         
-    def request_chunks(self, chunks : dict):
+    def request_chunks(self, request : dict):
 
-        req_chunks = json.dumps(chunks)
+        req_chunks = json.dumps(request)
 
         message = {
             "message_code" : 310,
@@ -44,10 +46,10 @@ class Client:
 
         msg = json.dumps(message)
 
-        self.conn.send(msg.encode("utf-8"))
+        self.server_conn.send(msg.encode("utf-8"))
 
         #TODO Receive entire message
-        response = self.conn.recv(1024)
+        response = self.server_conn.recv(1024)
         data = json.loads(response)
 
         print(data)
@@ -56,6 +58,41 @@ class Client:
             return data["peers"]
         elif data["message_type"] == 731:
             return None
+
+    def update_chunks(self, vid_name):
+
+        message = {
+            "message_code" : 410,
+            "message_comment" : "Update Chunks",
+            "vid_name" : vid_name,
+            "uploader_info" : None
+        }
+
+        msg = json.dumps(message)
+
+        self.server_conn.send(msg.encode("utf-8"))
+
+        #TODO Receive entire message
+        response = self.server_conn.recv(1024)
+        data = json.loads(response)
+
+        print(data)
+
+        if data["message_type"] == 641:
+            pass
+        elif data["message_type"] == 741:
+            pass
+    
+    def respond_to_heartbeat(self):
+
+        message = {
+            "message_code" : 101,
+            "message_comment" : "Alive"
+        }
+
+        msg = json.dumps(message)
+
+        self.server_conn.send(msg.encode("utf-8"))
 
 if __name__ == "__main__":
     client = Client()
@@ -69,4 +106,8 @@ if __name__ == "__main__":
 
     print(client.request_chunks(request))
 
-    client.conn.close()
+    client.avail_chunks["amogh.mp4"] = [0, 1, 2, 3, 4, 5, 6, 7]
+
+
+    
+    client.server_conn.close()
